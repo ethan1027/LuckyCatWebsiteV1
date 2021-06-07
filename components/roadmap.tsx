74b2154a9ab0dd831e485d3cf06a9e19709d995d
Ethan Tong <ethantong.studio@gmail.com>
2021-06-06 23:42:20 -0500

    fix font and color

--- components/roadmap.tsx
+++ components/roadmap.tsx
@@ -7,9 +7,9 @@ const Roadmap = (props) => (
       <div className="z-20 bg-yellow-400 flex items-center order-1 shadow w-8 h-8 rounded-full">
         <h1 className="mx-auto font-semibold text-lg">&nbsp;</h1>
       </div>
-      <div className="order-1 bg-yellow-400 rounded-lg shadow w-5/12 px-6 py-4 shadow-2xl transition duration-500 ease-in-out hover:shadow-yellow transform hover:-translate-y-2 hover:scale-110">
-        <h3 className="mb-3 font-bold text-xl">Lorem Ipsum</h3>
-        <p className="leading-snug tracking-wide text-gray-900 text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      <div className="order-1 bg-white rounded-lg shadow w-5/12 px-6 py-4 shadow-2xl transition duration-500 ease-in-out hover:shadow-yellow transform hover:-translate-y-2 hover:scale-110">
+        <h3 className="mb-3 font-bold text-2xl">Q2 2021</h3>
+        <p className="text-xl font-medium leading-snug tracking-wide text-gray-900 text-opacity-100">😊 $HAPPY Launch ✅</p>
       </div>
     </div>
 
@@ -18,9 +18,9 @@ const Roadmap = (props) => (
       <div className="z-20 bg-yellow-400 flex items-center order-1 shadow w-8 h-8 rounded-full">
         <h1 className="mx-auto font-semibold text-lg">&nbsp;</h1>
       </div>
-      <div className="order-1 bg-yellow-400 rounded-lg shadow w-5/12 px-6 py-4 shadow-2xl transition duration-500 ease-in-out hover:shadow-yellow transform hover:-translate-y-2 hover:scale-110">
-        <h3 className="mb-3 font-bold text-xl">Lorem Ipsum</h3>
-        <p className="font-medium leading-snug tracking-wide text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      <div className="order-1 bg-white rounded-lg shadow w-5/12 px-6 py-4 shadow-2xl transition duration-500 ease-in-out hover:shadow-yellow transform hover:-translate-y-2 hover:scale-110">
+        <h3 className="mb-3 font-bold text-2xl">Q3 2021</h3>
+        <p className="text-xl font-medium leading-snug tracking-wide text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
       </div>
     </div>
 
@@ -29,9 +29,9 @@ const Roadmap = (props) => (
       <div className="z-20 bg-yellow-400 flex items-center order-1 shadow w-8 h-8 rounded-full">
         <h1 className="mx-auto font-semibold text-lg">&nbsp;</h1>
       </div>
-      <div className="order-1 bg-yellow-400 rounded-lg shadow w-5/12 px-6 py-4 shadow-2xl transition duration-500 ease-in-out hover:shadow-yellow transform hover:-translate-y-2 hover:scale-110">
-        <h3 className="mb-3 font-bold text-gray-800 text-xl">Lorem Ipsum</h3>
-        <p className="leading-snug tracking-wide text-gray-900 text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      <div className="order-1 bg-white rounded-lg shadow w-5/12 px-6 py-4 shadow-2xl transition duration-500 ease-in-out hover:shadow-yellow transform hover:-translate-y-2 hover:scale-110">
+        <h3 className="mb-3 font-bold text-2xl">Q4 2021</h3>
+        <p className="text-xl font-medium leading-snug tracking-wide text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
       </div>
     </div>
 
@@ -40,9 +40,9 @@ const Roadmap = (props) => (
       <div className="z-20 bg-yellow-400 flex items-center order-1 shadow w-8 h-8 rounded-full">
         <h1 className="mx-auto font-semibold text-lg">&nbsp;</h1>
       </div>
-      <div className="order-1 bg-yellow-400 rounded-lg shadow w-5/12 px-6 py-4 shadow-2xl transition duration-500 ease-in-out hover:shadow-yellow transform hover:-translate-y-2 hover:scale-110">
-        <h3 className="mb-3 font-bold text-xl">Lorem Ipsum</h3>
-        <p className="font-medium leading-snug tracking-wide text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      <div className="order-1 bg-white rounded-lg shadow w-5/12 px-6 py-4 shadow-2xl transition duration-500 ease-in-out hover:shadow-yellow transform hover:-translate-y-2 hover:scale-110">
+        <h3 className="mb-3 font-bold text-2xl">Q1 2022</h3>
+        <p className="text-xl font-medium leading-snug tracking-wide text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
       </div>
     </div>
   </div>
--- components/tokenomics.tsx
+++ components/tokenomics.tsx
@@ -66,9 +66,9 @@ export default function Tokenomics(props) {
                   <div className="flex items-center justify-center h-12 w-12 rounded-md bg-yellow-400 text-white">
                     <feature.icon className="h-6 w-6" aria-hidden="true" />
                   </div>
-                  <p className="mt-5 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
+                  <p className="mt-5 text-xl leading-6 font-medium text-gray-900">{feature.name}</p>
                 </dt>
-                <dd className="mt-2 text-base text-gray-500">{feature.description}</dd>
+                <dd className="mt-2 text-lg text-gray-500">{feature.description}</dd>
               </div>
             ))}
           </dl>
