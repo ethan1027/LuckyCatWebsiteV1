d1f6c1ee3bf4867569d2447d8a6e84edd91dbb2d
anhhovn <hdma.vn@gmail.com>
2021-06-08 11:17:55 -0600

    footer

--- components/footer-media.tsx
+++ components/footer-media.tsx
@@ -1,10 +1,19 @@
 import {FaTwitter} from 'react-icons/fa'
 import {FaInstagram} from 'react-icons/fa'
+import {FaTelegramPlane} from 'react-icons/fa'
+import {FaRedditAlien} from 'react-icons/fa'
+import {FaDiscord} from 'react-icons/fa'
+import {FaTwitch} from 'react-icons/fa'
+
 export default function Medias(){
     return (
-        <div className="bg-white w-full text-3xl md:flex "> 
-            <a className="text-yellow-400" href="#"><FaTwitter/></a>
-            <a className="text-yellow-400" href="#"><FaInstagram/></a>
+        <div className=" w-full bg-yellow-100 bg-opacity-80 text-3xl md:flex flex justify-center  "> 
+            <a className="mx-2 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaTwitter/></a>
+            <a className="mx-2 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaInstagram/></a>
+            <a className="mx-2 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaTelegramPlane/></a>
+            <a className="mx-2 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaRedditAlien/></a>
+            <a className="mx-2 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaDiscord/></a>
+            <a className="mx-2 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaTwitch/></a>
             
         </div>
     )
