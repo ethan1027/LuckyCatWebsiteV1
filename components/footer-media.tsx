c54cb44f5138bb9975f9e30b2b16d601c1fcb344
anhhovn <hdma.vn@gmail.com>
2021-06-08 11:47:32 -0600

    footer banner fixed

--- components/footer-media.tsx
+++ components/footer-media.tsx
@@ -4,17 +4,24 @@ import {FaTelegramPlane} from 'react-icons/fa'
 import {FaRedditAlien} from 'react-icons/fa'
 import {FaDiscord} from 'react-icons/fa'
 import {FaTwitch} from 'react-icons/fa'
+import styled from "styled-components"
+
 
 export default function Medias(){
+   
     return (
-        <div className=" w-full bg-yellow-100 bg-opacity-80 text-3xl md:flex flex justify-center  "> 
-            <a className="mx-2 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaTwitter/></a>
-            <a className="mx-2 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaInstagram/></a>
-            <a className="mx-2 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaTelegramPlane/></a>
-            <a className="mx-2 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaRedditAlien/></a>
-            <a className="mx-2 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaDiscord/></a>
-            <a className="mx-2 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaTwitch/></a>
+        <div className=" py-2 w-full bg-yellow-100  text-2xl md:flex flex justify-center  "> 
+            <a className="mx-3 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaTwitter/></a>
+            <a className="mx-3 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaInstagram/></a>
+            <a className="mx-3 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaTelegramPlane/></a>
+            <a className="mx-3 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaRedditAlien/></a>
+            <a className="mx-3 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaDiscord/></a>
+            <a className="mx-3 py-2 text-yellow-400 hover:text-yellow-300" href="#"><FaTwitch/></a>
             
         </div>
+        
     )
+
+    
+
 }
